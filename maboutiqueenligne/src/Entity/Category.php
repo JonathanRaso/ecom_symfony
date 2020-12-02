a2595bc224e3474e274e7804f1746a55b9d8e50e
JonathanRaso <jonathanraso.dev@gmail.com>
2020-12-02 16:42:43 +0100

    Product entity mapped in EasyAdmin => can add product with image but can't update. Waiting for infos about that, and will fix it later

--- maboutiqueenligne/src/Entity/Category.php
+++ maboutiqueenligne/src/Entity/Category.php
@@ -34,6 +34,11 @@ class Category
         $this->products = new ArrayCollection();
     }
 
+    // Need to stringify our categories for EasyAdmin, so we create this function __toString()
+    public function __toString() {
+        return $this->getName();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
