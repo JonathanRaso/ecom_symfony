84285fcca20b702160ad54713a27210a5c8d9513
JonathanRaso <jonathanraso.dev@gmail.com>
2020-12-03 16:56:25 +0100

    New features inside the cart. Add, Decrease, Remove products from the cart

--- maboutiqueenligne/src/Classe/Cart.php
+++ maboutiqueenligne/src/Classe/Cart.php
@@ -2,15 +2,19 @@
 
 namespace App\Classe;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
     private $session;
+    private $entityManager;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(EntityManagerInterface $entityManager, SessionInterface $session)
     {
         $this->session = $session;
+        $this->entityManager = $entityManager;
     }
 
     public function add($id)
@@ -39,5 +43,63 @@ class Cart
     {
         return $this->session->remove('cart');
 
+    }
+
+    public function delete($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        // unset will delete the product with the right id inside our cart
+        unset($cart[$id]);
+
+        // We set the updated cart in our session and we return it
+        return $this->session->set('cart', $cart);
+    }
+
+    public function decrease($id)
+    {
+        // >We get data from our cart and put it inside $cart
+        $cart = $this->session->get('cart', []);
+
+        // We check if the quantity inside our cart is greater than 1
+        // If yes, we reduce the quantity by one
+        // If no, we remove the product from the cart
+        if ($cart[$id] > 1 ) {
+            $cart[$id]--;
+        } else {
+            unset($cart[$id]);
+        }
+
+        return $this->session->set('cart', $cart);
+    }
+
+    public function getFull()
+    {
+        // We create a variable ($cartComplete, an empty array) in order to
+        // Put informations about the products added to the cart
+        $cartComplete = [];
+
+        // Before going inside the foreach, we check if we have products inside our cart
+        // Otherwise, we will have an error
+        if ($this->get()) {
+            //  With this loop, we add the right product(with all its infos)
+            // And we add it inside our $cartComplete array
+            foreach ($this->get() as $id => $quantity) {
+                $product_object = $this->entityManager->getRepository(Product::class)->findOneById($id);
+                if (!$product_object) {
+                    $this->delete($id);
+                    // With continue, we skip this product and we continue the loop with every other products
+                    // We can ignore id linked to a product that doesn't exist and go through the loop with the other products
+                    // Useful if a user add a new product with the route cart/add/{id}. The fake product will not be added inside our cart
+                    continue;
+                }
+                $cartComplete[] = [
+                    'product' => $product_object, 
+                    'quantity' => $quantity
+                ];
+            }
+        }
+
+        return $cartComplete;
     }
 }
--- maboutiqueenligne/src/Controller/CartController.php
+++ maboutiqueenligne/src/Controller/CartController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
-use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,22 +22,8 @@ class CartController extends AbstractController
      */
     public function index(Cart $cart): Response
     {
-        // We create a variable ($cartComplete, an empty array) in order to
-        // Put informations about the products added to the cart
-        $cartComplete = [];
-
-        //  With this loop, we add the right product(with all its infos)
-        // And we add it inside our $cartComplete array
-        foreach ($cart->get() as $id => $quantity) {
-            $cartComplete[] = [
-                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id), 
-                'quantity' => $quantity
-            ];
-        }
-
         return $this->render('cart/index.html.twig', [
-            // Now, we give $cartComplete to the view, with all the right data needed
-            'cart' => $cartComplete
+            'cart' => $cart->getFull()
         ]);
     }
 
@@ -60,5 +45,25 @@ class CartController extends AbstractController
         $cart->remove();
 
         return $this->redirectToRoute('products');
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="delete_to_cart")
+     */
+    public function delete(Cart $cart, $id): Response
+    {
+        $cart->delete($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/cart/decrease/{id}", name="decrease_to_cart")
+     */
+    public function decrease(Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+
+        return $this->redirectToRoute('cart');
     }
 }
