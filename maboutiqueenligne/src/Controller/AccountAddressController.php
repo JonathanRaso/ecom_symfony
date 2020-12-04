1da7d6bf0db9a152a539f552cafc641552baa1a8
JonathanRaso <jonathanraso.dev@gmail.com>
2020-12-04 15:29:36 +0100

    Carrier and delivery address choice added

--- maboutiqueenligne/src/Controller/AccountAddressController.php
+++ maboutiqueenligne/src/Controller/AccountAddressController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Entity\Address;
 use App\Form\AddressType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -33,7 +34,7 @@ class AccountAddressController extends AbstractController
     /**
      * @Route("/compte/adresses/ajouter-une-adresse", name="account_address_add")
      */
-    public function add(Request $request): Response
+    public function add(Cart $cart, Request $request): Response
     {   
         $address = new Address();
         $form = $this->createForm(AddressType::class, $address);
@@ -44,6 +45,11 @@ class AccountAddressController extends AbstractController
             $address->setUser($this->getUser());
             $this->entityManager->persist($address);
             $this->entityManager->flush();
+            if ($cart->get()) {
+                return $this->redirectToRoute('order');
+            } else {
+                return $this->redirectToRoute('account_address');
+            }
             return $this->redirectToRoute('account_address');
         }
 
--- maboutiqueenligne/src/Controller/OrderController.php
+++ maboutiqueenligne/src/Controller/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,18 @@ class OrderController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('order/index.html.twig');
+        // If the user doesn't have any address, we redirect to the adding address page
+        if (!$this->getUser()->getAddresses()->getValues()) {
+            return $this->redirectToRoute('account_address_add');
+        }
+
+        // This form is not linked to a class, so we use null for 2nd args and not an instance of the class
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
